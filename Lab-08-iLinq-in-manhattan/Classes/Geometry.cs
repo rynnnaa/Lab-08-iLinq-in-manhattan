9411e6a70faa2354479824e671dcd2388048f90c
Tulip Pink <pinktulips@hotmail.com>
2019-01-20 20:43:11 -0800

    Had to start a new project technical issues

---
+++ Lab-08-iLinq-in-manhattan/Classes/Geometry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_08_iLinq_in_manhattan.Classes
+{
+    public class Geometry
+    {
+        public string type { get; set; }
+        public List<double> coordinates { get; set; }
+    }
+}
