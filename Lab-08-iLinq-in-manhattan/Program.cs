f04e2721a9915d8d502e5731f4aea487ee87dcbc
Tulip Pink <pinktulips@hotmail.com>
2019-01-20 20:55:36 -0800

    print neighborhoods

--- Lab-08-iLinq-in-manhattan/Program.cs
+++ Lab-08-iLinq-in-manhattan/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Lab_08_iLinq_in_manhattan.Classes;
+using Newtonsoft.Json;
+using System;
 using System.IO;
-
+using System.Linq;
 
 namespace Lab_08_iLinq_in_manhattan
 {
@@ -23,6 +25,22 @@ namespace Lab_08_iLinq_in_manhattan
             {
                 text = reader.ReadToEnd();
             }
+
+            //set up the deserialized object
+            var obj = JsonConvert.DeserializeObject<RootObject>(text);
+
+            //1. print all neighborhoods
+            var allneighborhoods = obj.Features.Select(f => f.Properties.Neighborhood);
+
+            Console.WriteLine();
+            Console.WriteLine("======All NeighborHoods======");
+            Console.WriteLine();
+
+            foreach (var prop in allneighborhoods)
+            {
+                Console.WriteLine(prop);
+            }
         }
+
     }
 }
